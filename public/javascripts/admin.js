d8268c8ea8be6b25f876e19682c345a6d8fbbffa
Roman Sobchuk <RomanSobchuk@coherentsolutions.com>
2021-05-26 19:30:27 +0300

    345

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,12 +1,20 @@
 var videoExt = ['JPEG', 'JPG', 'PNG', 'GIF', 'SVG']
 var imgExt = ['MP4', 'M4P', 'M4V', 'GIF', 'MPG', 'MPEG', 'MPV', 'MP2', 'AVI', 'WMV']
+var modal;
 
 function init() {
     document.onclick = onClickInit;
+    document.onchange = onChangeInit;
+    modal = new bootstrap.Modal(document.getElementById('warning-modal'), { });
 }
 
 function onClickInit(e) {
     editCread(e);
+    addProject(e);
+}
+
+function onChangeInit(e) {
+    onChangedAddProjectForm(e);
 }
 
 function editCread(e) {
@@ -14,11 +22,11 @@ function editCread(e) {
     let td = e.target.parentElement.previousElementSibling;
     td.firstChild.classList.add('hid');
     td.lastChild.classList.remove('hid');
-    activeSubmit();
+    activeSubmit('save-cred');
 }
 
-function activeSubmit() {
-    let btn = document.getElementById('save-cred');
+function activeSubmit(id) {
+    let btn = document.getElementById(id);
     if (!btn.getAttribute('disabled')) return;
 
     btn.removeAttribute('disabled');
@@ -26,19 +34,36 @@ function activeSubmit() {
     btn.classList.remove('btn-outline-secondary');
 }
 
-// $('#project > div > button:nth-child(1)').click((el) => {
-// 	$('#addProj').removeClass('hid');
-// 	$(el.currentTarget).addClass('hid');
-// });
+function addProject(e) {
+    if(e.target.id !== 'add-project') return;
+    let form = document.getElementById('add-project-form');
+    form.classList.remove('hid');
+}
+
+function onChangedAddProjectForm(e) {
+    if (!hasClass(e.target, 'add-project')) return;
+    let inputs = Array.from(document.querySelectorAll('.add-project'));
+    let isValid = true;
+    let hasCorrectPh = true;
+    let hasCorrectVd = true;
+    let hasFiles = false;
+    inputs.forEach(input => {
+        if(input.type === 'file') {
+            hasFiles = hasFiles ? hasFiles : input.files.length > 0;
+            if (input.id === 'photos') {
+                Array.from(input.files).forEach(file => {
+                    let ext = file.name.split('.').pop();
+                });                
+            } else if (input.id === 'videos') {
+                
+            }
+        } else {
+            isValid = input.value !== '';
+        }
+    });
+    // modal.toggle();
+}
 
 function hasClass(item, name) {
     return item.classList.contains(name);
-}
-
-function toggleClass(item, name) {
-    if (!hasClass(item, name)) {
-        item.classList.add(name);
-    } else {
-        item.classList.remove(name);
-    }
 }
